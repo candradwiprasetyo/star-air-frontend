f5ea29541fae0ece7b2f931beb53f30c1dcbc74c
candra dwi prasetyo <candradwiprasetyo@gmail.com>
2022-03-11 21:48:24 +0700

    add account

--- components/account/Overview.vue
+++ components/account/Overview.vue
@@ -6,32 +6,44 @@
           <div class="text-lg font-semibold font-noto-sans text-grayscale-900">Overview</div>
         </div>
       </div>
-      <div class="p-6">
+      <div class="p-6 overflow-hidden">
         <div class="flex mb-4">
           <div class="flex-1 text-lg font-semibold font-noto-sans text-grayscale-900">Overview</div>
           <div class="flex-1 font-semibold text-right font-noto-sans text-secondary-900">Read More ></div>
         </div>
-        <table class="table-auto md:w-full custom-table w-[1200px]">
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Type</th>
-              <th>Star Points</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="index in 5" :key="index">
-              <td>19 November 2021</td>
-              <td>Transfer Points for Plus (+)</td>
-              <td>+1000</td>
-            </tr>
-          </tbody>
-          <tfoot>
-            <tr>
-              <th colspan="3">&nbsp;</th>
-            </tr>
-          </tfoot>
-        </table>
+        <div
+          class="
+            mt-6
+            md:px-0 md:-mx-0
+            px-6
+            -mx-6
+            md:w-full
+            w-[100vw]
+            overflow-auto
+          "
+        >
+          <table class="table-auto md:w-full custom-table w-[1200px]">
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Type</th>
+                <th>Star Points</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="index in 5" :key="index">
+                <td>19 November 2021</td>
+                <td>Transfer Points for Plus (+)</td>
+                <td>+1000</td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <th colspan="3">&nbsp;</th>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
       </div>
     </div>
   </div>
