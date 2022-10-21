853af4c7ec0b64cfad70af2603ddc90f9ffc30d3
cahaya pangripta alam <cahayaalam19@gmail.com>
2022-10-21 17:28:53 +0700

    change title and desc register using cms

--- pages/registration-full.vue
+++ pages/registration-full.vue
@@ -15,17 +15,10 @@ export default {
     <MainMenu />
     <div class="container px-6 py-6 mx-auto md:py-14 md:px-0">
       <div class="text-3xl font-semibold text-grayscale-900 font-noto-sans">
-        Create New Account
+        {{(getDataContent('title_create_new_account')) ? getDataContent('title_create_new_account') : ''}}
       </div>
       <div class="mt-3 text-sm text-grayscale-500 md:text-base">
-        Enjoy hundreds of benefits when you joining Star Air loyalty program.
-        earn points when you flight, stay at hotel, and buy goods at Star Air
-        duty free and redeem it for your next travel. Lorem ipsum dolor sit
-        amet, consectetur adipiscing elit. Sed ac mauris vitae sem posuere
-        blandit. Nam pretium venenatis bibendum. Nam eget ante eu metus
-        consequat semper non ac nunc. Duis semper massa nec tellus lacinia, id
-        semper neque lobortis. Donec sit amet eros leo. Sed lacinia vestibulum
-        lorem, vel imperdiet ipsum.
+        {{(getDataContent('desc_create_new_account')) ? getDataContent('desc_create_new_account') : ''}}
       </div>
       <div class="block p-6 mt-10 md:hidden bg-light-blue rounded-xl">
         <div class="text-lg font-semibold text-grayscale-900">
